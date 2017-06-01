c1d59b53c67ae667c690d48417dc3f98d814aec4
Tamara Temple <tamara.temple@reachlocal.com>
2017-06-01 16:44:26 -0500

    finish talk "slides" and add starter pack

--- README.md
+++ README.md
@@ -1,14 +1,143 @@
+#
+
 # Talk: Command Line Aliases
 
-- Speaker: Tamara Temple <tamara@tamouse.org>
+- Speaker: Tamara Temple <tamara@tamouse.org> <@tamouse>
+
+- Repo: <https://github.com/tamouse/Talk-Command-Line-Aliases>
+
+
+
+
+-----
 
 ## Introduction
 
+What are aliases? Basically, single word commands that execute a more
+complex or hard-to-remember command line.
+
+An alias is a string that's assigned to a word with the `alias`
+command. (yes, commands defining commands!) Another way to think of an
+alias is a simple macro, but for the command line.
+
+-----
+
+## Starting up
+
+First, launch your command line (aka terminal) application:
+
+* on macOS: Terminal.app (found under /Application/Utilities)
+* on windows: Git-Bash (or if running Win10, you could be running
+  Ubuntu bash)
+* on Linux: gnome-terminal or xterm or ...
+
+When the terminal app starts up, it should drop you into your home
+directory. (Except Git-Bash drops you into the root.)
+
+To make sure, enter:
+
+    cd
+
+This is the "change directory" command and entering by itself will
+take you to your HOME directory.
+
+-----
+
 ## `.bash_profile`
+
+Once in your HOME directory, enter the following command:
+
+    touch .bash_profile
+
+This file is *sourced* whenever you start a terminal app, which runs as a
+"Login Shell". A file being *sourced* is a little different than
+executing the same file. The main upshot that, when it ends, all the
+things it did are available in your working shell.
+
+The `.bash_profile` is where we'll start to add aliases, so you want
+to pull it up in an editor.
+
+The `vim` and `nano` editors are available all over, but you can also
+use any of the other code editors if you'd rather. I'll be using vim.
+
+-----
 
 ## `alias`
 
-## Starter collection
+The `alias` command consists of two parts:
+
+- the alias word
+- the alias string
+
+**Example:**
+
+    alias ll='ls -alF'
+
+- `ll` is the alias word
+- `'ls -alF'` is the alias string
+
+The equals `=` sign connects them.
+**Make sure there are NO spaces around the equals sign.**
+
+-----
+
+## Loading aliases from `.bash_profile`
+
+Once you have that defined in the `.bash_profile`, save it, and go
+back to your terminal.
+
+Then run the following command at the prompt:
+
+    source .bash_profile
+
+and then type:
+
+    alias
+
+And you should see your alias in the listing.
+
+You don't normally have to source your .bash_profile as it should
+automatically run with you start your terminal app. Just do so when
+you change something in it.
+
+-----
+
+## Running your newly created alias
+
+Again, at the terminal app prompt, type:
+
+    ll
+
+and if everything works, you'll see a full (aka "long") directory
+listing, with directories marked with a `/` at the end of their name.
+
+-----
+
+## Bash functions
+
+Bash functions are close cousins of bash aliases. They are a little
+bit more than a single line of code, and let you pass in parameters.
+
+The following function abbreviates the "Git Dance" to a single command
+word:
+
+    gacp () {
+      git add --all --verbose
+      git commit -m "$1"
+      git push -u origin HEAD
+    }
+
+Calling it with an argument provides the commit message:
+
+    gacp 'YOLO'
+
+-----
+
+## Starter Kit
+
+I've provided a starter set of bash aliases and functions that you
+might find helpful in the `starter_pack.sh` file in the repo.
 
 ## Add your own!
 
+Fork the repo, add yours, submit a PR and share!
--- starter_pack.sh
+++ starter_pack.sh
@@ -17,6 +17,142 @@ gacp() {
   git push -u origin HEAD
 }
 
-
-# If you are in git-bash, you might want this one:
+# If you are in git-bash on Windows, you might want this one:
 alias more=less
+
+## Some basic aliases:
+# make standard file manipulation commands verbose
+alias mv='mv -v'
+alias cp='cp -v'
+alias rm='rm -v'
+alias tac="tail -r "
+
+# Capture the location of the ls command
+export LSCMD=$(which ls)
+# Depending on the system, set the colour options
+if [ "$system_type" == "Darwin" ] ; then
+  # "Darwin" is macOS
+  export LS_OPTS="-G"
+else
+  export LS_OPTS="--color=auto"
+fi
+
+alias ls="$LSCMD -AFGh $LS_OPTS"
+alias ll="$LSCMD -AFGlh $LS_OPTS"
+alias l1="$LSCMD -1h $LS_OPTS"
+alias lm="$LSCMD -mh $LS_OPTS"
+
+alias now='date "+%T"'
+
+# This one works on Linux
+alias ports='netstat -tulanp'
+
+# These work on macOS
+alias openip='lsof -Pnl +M' # open IP connections (4 or 6)
+alias openip4='lsof -Pnl +M -i4' # open IPv4 connections
+alias openip6='lsof -Pnl +M -i6' # open IPv6 connections
+
+alias cpan='echo use cpanm instead'
+
+alias phplint='php -l'
+alias phplintdir='for f in $(find . -name "*.php") ; do php -l "$f"; done'
+
+alias ping1='ping -c 1'
+alias timestamp='date "+%Y-%m-%d-%H-%M-%S"'
+
+alias s3sync='s3cmd -r --rexclude-from=$HOME/.s3sync-excludes sync '
+
+function tree () {
+  /bin/ls -AR | grep ':$' | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
+}
+
+# replace spaces in file names with underscores
+function unspace_filename () {
+  echo "$1" | tr --squeeze-repeats " " "_"
+}
+
+# lowercase file names
+function lc_filename () {
+  echo "$1" | tr '[:upper:]' '[:lower:]'
+}
+
+# clean file name: lower case, no spaces, alphanumeric plus _-+. only
+function clean_filename () {
+  echo "$1" | perl -p -e 'chomp;s/\s+/_/g;s/[^[:alnum:]_+\.-]*//g;lc;'
+}
+
+# find diff between backup and current files in pwd after a filtering operation
+function diffbak () {
+  for f in *.bak ; do
+	  diff "${f%%.bak}" "$f"
+  done
+}
+
+# push back original files in pwd after filtering operation
+function restore () {
+  for f in *.bak ; do
+	  mv -v "$f" "${f%%.bak}"
+  done
+}
+
+# rename old files in pwd with new extension
+function rename_all () {
+  for f in *.$1 ; do
+	  mv "$f" "${f%%.$1}.$2"
+  done
+}
+
+function psgrep() {
+  ps auxww | grep -v grep | grep --color=auto -E "(^USER|$*)"
+}
+
+alias clean_photos='for f in *; do chmod -x $f ; mv $f $(echo $f|tr "A-Z_" "a-z-"); done'
+
+function dl() {
+  echo $(date "+%FT%T") "$PWD" ':' "$@" >> $HOME/.dev.log
+}
+
+## Interesting bash function for setting up a new front-end project
+# Usage: new_project DIRNAME DESCRIPTION
+function new_project() {
+  git init "$1" && \
+	  pushd "$1" && \
+	  echo "$2" > README.txt && \
+	  echo "$2" > .git/description && \
+	  echo "/node_modules/" >> .gitignore && \
+	  hub create -d "$2" && \
+	  yarn init && \
+	  gacp initial
+}
+
+## A whole pile of git aliases and functions:
+alias gaa='git add --all -v'
+alias gb='git branch'
+alias gbls='git branch -av'
+alias gcl='git clone'
+alias gcm='git commit -m'
+alias gco='git checkout'
+alias gcob='git checkout -b'
+alias gd='git diff'
+alias gdoh='git diff origin HEAD'
+alias gl='git log'
+alias glg='git log --graph --pretty=format":%C(yellow)%h%Cblue%d%Creset %s %C(white) %an,%ar%Creset" --abbrev-commit --decorate'
+alias glgh='git log --graph --pretty=format":%C(yellow)%h%Cblue%d%Creset %s %C(white) %an,%ar%Creset" --abbrev-commit --decorate | head'
+alias glo='git log --oneline --decorate'
+alias glog='git log'
+alias gloh='git log --oneline --decorate | head'
+alias gls='git ls-files'
+alias gpl='git pull'
+alias gploh='git pull origin HEAD'
+alias gps='git push'
+alias gpsoh='git push -u origin HEAD'
+alias gpsfoh='git push -fu origin HEAD'
+alias gpsfuoh='git push -fu origin HEAD'
+alias gr='git remote'
+alias grls='git remote -v'
+alias gyolo='git add --all && git commit -m YOLO && git push -fu origin HEAD'
+
+
+## I use this to make we-sized images in a "web" subdirectory
+# Usage: mkweb *.png *.jpg [...]
+alias mkweb='mkdir -p web; mogrify -verbose -path web -format jpg -resize 1024 -quality 60 '
